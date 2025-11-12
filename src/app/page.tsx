fefb4dc7b8132f5de260a5f82f44deea39b83817
Abhishek <abhishekparmar162@gmail.com>
2025-11-12 15:59:33 +0530

    auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div>
       <Button>Click me</Button>
+      <SignInButton>Login</SignInButton>
     </div>
   );
 };
